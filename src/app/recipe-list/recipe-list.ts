66fe36494b0dc41ed1bb4fc6da647535ad1c0228
Taiwo Ademola Sunday <taiwoademola065@gmail.com>
2026-08-27 15:38:52 +0100

    feat: add recipe details and list components with associated styles and tests
    - Implemented RecipeDetails component with HTML, CSS, and spec files.
    - Implemented RecipeList component with HTML, CSS, and spec files.
    - Created Recipe model to define the structure of recipe data.
    - Updated app configuration to include HttpClient for API interactions.

---
+++ src/app/recipe-list/recipe-list.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-recipe-list',
+  imports: [],
+  templateUrl: './recipe-list.html',
+  styleUrl: './recipe-list.css',
+})
+export class RecipeList {}
