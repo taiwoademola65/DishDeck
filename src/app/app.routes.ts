b442693e3d1744f3b0d301822031b13e32b8ec74
Taiwo Ademola Sunday <taiwoademola065@gmail.com>
2026-09-06 18:59:20 +0100

    feat: add error page component and routing; implement loading state in home component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { Home } from './home/home';
 import { RecipeList } from './recipe-list/recipe-list';
 import { RecipeDetail } from './recipe-detail/recipe-detail';
+import { ErrorPage } from './error-page/error-page';
 
 export const routes: Routes = [
   {path: '',  component: Home},
   {path: 'recipeList', component: RecipeList},
-  {path: 'recipe/:id', component: RecipeDetail}
+  {path: 'recipe/:id', component: RecipeDetail},
+  {path: '**', component: ErrorPage}
 ];
