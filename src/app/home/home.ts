d0deb56daa30d9d82609baba72478dd16dc71fd9
Taiwo Ademola Sunday <taiwoademola065@gmail.com>
2026-09-06 12:08:20 +0100

    feat: refactor home component; remove unused imports and update recipe fetching method feat: update navbar links; change routerLink to href for Fresh Recipes

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,19 +1,18 @@
-import { Component, inject, signal } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { Navbar } from '../navbar/navbar';
 import { Footer } from '../footer/footer';
-import { RouterLink } from "@angular/router";
+import { RouterLink } from '@angular/router';
 import { RecipeService } from '../services/recipe-service';
-import { Recipe } from '../models/recipe.model';
+import { toSignal } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-home',
-  imports: [Navbar, Footer, RouterLink, ],
+  imports: [Navbar, Footer, RouterLink],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
 export class Home {
-  recipeService =  inject(RecipeService);
+  recipeService = inject(RecipeService);
 
-  recipes = signal<Recipe[]>([]);
-  
+  recipeResponse = toSignal(this.recipeService.getRecipes());
 }
